68c5e0cc076c97387c867198b18ae9d885f583cf
Sr-Van <vanderson.dev1@gmail.com>
2025-09-06 18:43:39 -0300

    feat: creating the lists and Listbox, adding to the frame

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import Listbox
 
 class App:
   def __init__(self):
@@ -8,6 +9,18 @@ class App:
 
     self.frame = tk.Frame(self.root)
     self.frame.grid()
+
+    #dicionario temporaria
+    self.lista_produtos = {"hamburguer": 10.0, "pizza": 15.0, "suco": 5.0}
+    self.pedido = {}
+    self.total_pedido = 0
+
+    self.listbox_produtos = Listbox(self.frame)
+    self.listbox_produtos.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+
+    self.listbox_pedido = Listbox(self.frame)
+    self.listbox_pedido.grid(row=2, column=5)
+
     
     self.root.mainloop()    
 
