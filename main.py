3efefc130745c30760f348d73148ebe477880020
Sr-Van <vanderson.dev1@gmail.com>
2025-09-14 22:43:01 -0300

    feat: refactor UI and styling of the order system
    This commit restructures the order interface to improve element distribution and visual aesthetics.
    
    - Creates separate frames to better organize buttons and other widgets.
    - Implements custom styles with ttk.Style for buttons, labels, and lists.
    - Adjusts how the total order value is displayed and updated.
    - Updates the appearance of all buttons, labels, and listboxes to follow the new style pattern.

--- main.py
+++ main.py
@@ -1,58 +1,89 @@
 import tkinter as tk
-from tkinter import Listbox, messagebox, simpledialog, ttk
+from tkinter import Listbox, messagebox, simpledialog, ttk, font
 from ttkthemes import ThemedTk
 
 class App:
   def __init__(self):
-    self.root = ThemedTk(theme="breeze")
+    self.root = ThemedTk(theme="arc")
     self.root.attributes('-fullscreen', True)
     self.root.title('APP - Sabor Rapido')
+    self.root.configure(bg="#f0f0f0")
 
-    #bind root to toogle or end fullscreen if needed
+    # -- binds --
     self.root.bind("<F11>", self.toggle_fullscreen)
     self.root.bind("<Escape>", self.end_fullscreen)
+
+    # -- control vars --
     self.isFullscreen = False
 
-    self.frame = ttk.Frame(self.root)
-    self.frame.grid()
-
+    # -- custom styles --
     self.style = ttk.Style(self.root)
-    self.style.configure('Custom.TButton', background='blue', foreground='black', font=('Arial', 12))
+    self.style.configure('Custom.TButton', foreground='black', font=('Arial', 16))
+    self.style.configure('Custom.TFrame', background="#f0f0f0", foreground='black', font=('Arial', 16))
+    self.style.configure('Custom.TLabel' ,background='#f0f0f0', foreground='black', font=('Arial', 16))
+    self.custom_font = font.Font(family='Arial', size=16)
+
+    # -- frames --
+    
+    self.upper_buttons = ttk.Frame(self.root, style='Custom.TFrame', height=150)
+    self.upper_buttons.pack(side='top', fill='x', padx=15, pady=10)
+    self.upper_buttons.pack_propagate(False)
+
+    self.lower_buttons = ttk.Frame(self.root, style='Custom.TFrame', height=150)
+    self.lower_buttons.pack(side='bottom', fill='x', padx=15, pady=10)
+    self.lower_buttons.pack_propagate(False)
+
+    self.middle_frame = ttk.Frame(self.root, style='Custom.TFrame')
+    self.middle_frame.pack(side='top', fill='both', expand=True, padx=15, pady=10)
+
+    self.left_frame = ttk.Frame(self.middle_frame, style='Custom.TFrame')
+    self.left_frame.pack(side='left', fill='both', expand=True, padx=15, pady=10)
+
+    self.right_frame = ttk.Frame(self.middle_frame, style='Custom.TFrame')
+    self.right_frame.pack(side='right', fill='both', expand=True, padx=15, pady=10)
+
 
     #dicionario temporaria
     self.lista_produtos = {"hamburguer": 10.0, "pizza": 15.0, "suco": 5.0}
     self.pedido = {}
     self.total_pedido = 0
 
-    self.listbox_produtos = Listbox(self.frame)
-    self.listbox_produtos.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+    # -- top --
 
-    self.listbox_pedido = Listbox(self.frame)
-    self.listbox_pedido.grid(row=3, column=5)
+    self.btn_criar_prod = ttk.Button(self.upper_buttons, text='Novo Produto (F1)',command=self.adicionar_produto, style='Custom.TButton', width=20)
+    self.btn_criar_prod.pack(side='left', fill='y', ipadx=10, ipady=10, padx=10, pady=10)
+
+    self.btn_confirm = ttk.Button(self.upper_buttons, text='Confirmar (F10)', command=self.emitir_recibo, style='Custom.TButton', width=20)
+    self.btn_confirm.pack(side='right', fill='y', ipadx=10, ipady=10, padx=10, pady=10)
+
+    # -- bottom --
+
+    self.btn_adicionar = ttk.Button(self.lower_buttons, text='Adicionar (ENTER)', command=self.adicionar_ao_pedido, style='Custom.TButton', width=20)
+    self.btn_adicionar.pack(side='left', fill='y',ipadx=10, ipady=10, padx=10, pady=10)
+
+    self.btn_remove = ttk.Button(self.lower_buttons, text='Remover (DELETE)',  command=self.remover_do_pedido, style='Custom.TButton', width=20)
+    self.btn_remove.pack(side='right', fill='y',ipadx=10, ipady=10, padx=10, pady=10)
+
+    #-- left --
+
+    self.label_titulo = ttk.Label(self.left_frame, text='Lista de produtos', style='Custom.TLabel')
+    self.label_titulo.pack(side='top', fill='x', ipadx=25, ipady=25)
+
+    self.listbox_produtos = Listbox(self.left_frame, width=20, height=10, bg='#ffecb7', fg='black', font=self.custom_font, selectbackground='orange', selectforeground='black', relief='ridge')
+    self.listbox_produtos.pack( fill='both', expand=True, ipadx=10, ipady=10)
+
+    self.label_total = ttk.Label(self.left_frame, text=f'TOTAL: {self.total_pedido}', style='Custom.TLabel')
+    self.label_total.pack(side='bottom', fill='x', ipadx=25, ipady=25)
+
+    #-- right --
+
+    self.label_pedido = ttk.Label(self.right_frame, text='Pedido:', style='Custom.TLabel')
+    self.label_pedido.pack(side='top', fill='x', ipadx=25, ipady=25)
+
+    self.listbox_pedido = Listbox(self.right_frame, width=20, height=10, bg='#ffecb7', fg='black', font=self.custom_font, selectbackground='orange', selectforeground='black', relief='ridge')
+    self.listbox_pedido.pack( fill='both', expand=True, ipadx=10, ipady=10)
 
     self.atualizar_lista_menu()
-
-    self.label_titulo = ttk.Label(self.frame, text='Lista de produtos')
-    self.label_titulo.grid(row=2, column=0, ipadx=12, ipady=12)
-
-    self.label_hold = ttk.Label(self.frame, text="")
-    self.label_hold.grid(row=4, column=0, ipadx=25, ipady=25)
-
-    self.label_pedido = ttk.Label(self.frame, text='Pedido:')
-    self.label_pedido.grid(row=2, column=5, ipadx=12, ipady=12)
-
-    self.btn_criar_prod = ttk.Button(self.frame, text='Novo Produto',command=self.adicionar_produto, style='Custom.TButton')
-    self.btn_criar_prod.grid(row=0, column=0, columnspan=2, ipadx=20, ipady=20)
-
-    self.btn_adicionar = ttk.Button(self.frame, text='Adicionar', command=self.adicionar_ao_pedido, style='Custom.TButton')
-    self.btn_adicionar.grid(row=5, column=0, columnspan=2, ipadx=20, ipady=20)
-
-    self.btn_remove = ttk.Button(self.frame, text='Remover',  command=self.remover_do_pedido, style='Custom.TButton')
-    self.btn_remove.grid(row=5, column=5, columnspan=2, ipadx=20, ipady=20)
-    
-    self.btn_confirm = ttk.Button(self.frame, text='Confirmar', command=self.emitir_recibo, style='Custom.TButton')
-    self.btn_confirm.grid(row=0, column=5, columnspan=2, ipadx=20, ipady=20)
-
     self.root.mainloop()     
 
   def atualizar_lista_menu(self):
@@ -122,9 +153,9 @@ class App:
     for produto, valor in self.pedido.items():
       print(f'Calculando {produto} no preco de {valor}')
       self.total_pedido+= valor
-
-    self.label_total = ttk.Label(self.frame, text=f'Total: {self.total_pedido}')
-    self.label_total.grid(row=4, column=0, ipadx=25, ipady=25)
+    
+    self.label_total.config(text=f'TOTAL: {self.total_pedido}')
+    self.label_total.update_idletasks() 
     
   def emitir_recibo(self):
     pedido = ''
