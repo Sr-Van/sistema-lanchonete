7951a5c4f26f54cdfc740ee54c6ae62e274e4bb6
Sr-Van <vanderson.dev1@gmail.com>
2025-09-17 11:04:43 -0300

    feat(db): get product and price from database

--- model/model.py
+++ model/model.py
@@ -2,11 +2,15 @@ import sqlite3
 
 
 class Database:
-  def __init__(self):
-    self.conn = sqlite3.connect("./database.db")
+  def __init__(self, db_name):
+    self.conn = sqlite3.connect(db_name)
     self.cursor = self.conn.cursor()
     self.create_table()
-    self.cursor.close()
+    self.insert_product('Coca-Cola', 5.99)
+    self.insert_product('Fanta', 5.99)
+    self.insert_product('Guarana', 5.99)
+    self.insert_product('Sprite', 5.99)
+    print(self.get_products())
     self.conn.close()
 
   def create_table(self):
@@ -29,6 +33,17 @@ class Database:
     except Exception as e:
       print(e)
 
+  def get_products(self):
+    try:
+      self.cursor.execute("SELECT produto, preco FROM produtos")
+      lista_produtos = self.cursor.fetchall()
+      dicionario = {}
+      for i in range(len(lista_produtos)):
+        dicionario[lista_produtos[i][0]] = lista_produtos[i][1]
+
+      return dicionario
+
+    except sqlite3.Error as e:
+      print(f'Erro ao buscar produtos: {e}')
 if __name__ == "__main__":
-  db = Database()
-  db.insert_product("Produto 3", 10.99)
+  db = Database('./database.db')
